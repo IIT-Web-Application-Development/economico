443f4e73816b3c78a2fb75e875fa79697320f4b2
milad <milad.sabouri@gmail.com>
2017-11-21 17:20:29 -0600

    Get Specific User

--- routes/users.js
+++ routes/users.js
@@ -7,10 +7,11 @@ var User = require('../models/user');
 /* GET users listing. */ 
 router.route('/')
 .get(function(req, res, next) {
-  res.send('return the users');
+  //res.send('return the users');
+  console.log('Getting Users');
 })
 .post(function(req, res, next) {
-  console.log('Add User');
+  console.log('Adding User');
   
   var info = req.body;
   var user = new User();
@@ -35,7 +36,27 @@ router.route('/')
 
 router.route('/:userid')
 .get(function(req, res, next){
-  res.send('return single user');
+  console.log('Getting specific user');
+  var userid = req.params.userid;
+  var user = {};
+  User.find({'_id': userid}, function(err, usr){
+      if(err){
+        console.log("ERROR in finding user: " + err);
+        res.status(500).json({'message':'Internal Error in Finding User'});
+      } else if ( usr.length !== 0){
+        console.log(usr);
+        user._id = usr[0]._id;
+        user.name = usr[0].name;
+        user.pass = usr[0].pass;
+        user.limit = usr[0].limit;
+        user.email = usr[0].email;
+        user.costs = usr[0].costs;
+        res.status(200).json(user);
+      } else{
+        console.log("User Not Found!");
+        res.status(200).json({});
+      }
+  });
 })
 .put(function(req, res, next){
   res.send('update single user');
