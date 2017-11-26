ab42903053313374d02e9262eb31b580da2cfb6a
Grese Hyseni <ghyseni@hawk.iit.edu>
2017-11-26 01:15:38 -0600

    Add dashboard route to display user data

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,50 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user');
 
-/* GET home page. */
+/* GET login page. */
 router.get('/login', function(req, res, next) {
   console.log('Login page');
   res.render('login', {});
 });
 
-/* GET home page. */
+/* GET register page. */
 router.get('/register', function(req, res, next) {
   console.log('Register page');
   res.render('register', {});
 });
+
+/* Get dashboard */
+router.get('/dashboard/:userId', function(req, res, next) {
+  console.log('Getting user dashboard');
+  var userId = req.params.userId;
+  var user = {};
+  User.find({
+    '_id': userId
+  }, function(err, usr) {
+    if (err) {
+      console.log("ERROR in finding user: " + err);
+      res.status(500).json({
+        'message': 'Internal Error in Finding User'
+      });
+    } else if (usr.length !== 0) {
+      user._id = usr[0]._id;
+      user.name = usr[0].name;
+      user.pass = usr[0].pass;
+      user.limit = usr[0].limit;
+      user.email = usr[0].email;
+      user.costs = usr[0].costs;
+      res.render('index', {
+        user: user
+      });
+    } else {
+      console.log("User Not Found!");
+      res.status(404).json({
+        "message": 'User Not Found'
+      });
+    }
+  });
+});
+
 
 module.exports = router;
