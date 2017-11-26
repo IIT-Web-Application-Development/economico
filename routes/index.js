36225b59f627892683c4b81a26a1247ec59eb00c
Grese Hyseni <ghyseni@hawk.iit.edu>
2017-11-25 23:38:17 -0600

    Remove route / temporary

--- routes/index.js
+++ routes/index.js
@@ -2,26 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-
-  console.log('Getting Users');
-  User.find({}, function(err, users) {
-    if (err) {
-      console.log("ERROR in finding user: " + err);
-      res.status(500).json({
-        'message': 'Internal Error in Finding User'
-      });
-    } else if (users.length !== 0) {
-      res.render('index', {
-        users: users
-      });
-    } else {
-      console.log("User Not Found!");
-      res.status(200).json({});
-    }
-  });
-
-/* GET home page. */
 router.get('/login', function(req, res, next) {
   console.log('Login page');
   res.render('login', {});
