e472b704ffd9590288f9984a521dd1e9b514da9c
Grese Hyseni <ghyseni@hawk.iit.edu>
2017-11-29 04:37:12 -0600

    Merge changes: categories/total with login functionality

--- routes/index.js
+++ routes/index.js
@@ -8,23 +8,25 @@ router.get('/login', function(req, res, next) {
   res.render('login', {});
 });
 
-router.post('/login', function(req,res,next){
-  User.authenticate(req.body.username, req.body.password, function (error, user) {
-      if (error || !user) {
-        console.log("There is an error");
-        res.status(401).send();
-      } else {
-        req.session.userId = user._id;
-        res.status(200).send({"userId":user._id});
-      }
-    });
+router.post('/login', function(req, res, next) {
+  User.authenticate(req.body.username, req.body.password, function(error, user) {
+    if (error || !user) {
+      console.log("There is an error");
+      res.status(401).send();
+    } else {
+      req.session.userId = user._id;
+      res.status(200).send({
+        "userId": user._id
+      });
+    }
+  });
 });
 
 router.get('/logout', function(req, res, next) {
   if (req.session) {
     // delete session object
     req.session.destroy(function(err) {
-      if(err) {
+      if (err) {
         return next(err);
       } else {
         return res.redirect('/login');
@@ -41,35 +43,88 @@ router.get('/register', function(req, res, next) {
 
 /* Get dashboard */
 router.get('/dashboard/:userId', function(req, res, next) {
-   if (req.session && req.session.userId) {
+
+  //Categories array of objects
+  var categories = [{
+      name: "Education",
+      color: '#3c8dbc',
+      total: 0,
+      icon: 'ion-ios-book'
+    },
+    {
+      name: "Groceries",
+      color: '#f39c12',
+      total: 0,
+      icon: 'ion-bag'
+    },
+    {
+      name: "Clothing",
+      color: '#00a65a',
+      total: 0,
+      icon: 'ion-tshirt-outline'
+    },
+    {
+      name: "Bills",
+      color: '#dd4b39',
+      total: 0,
+      icon: 'ion-ios-paper-outline'
+    },
+    {
+      name: "Travel",
+      color: '#00c0ef',
+      total: 0,
+      icon: 'ion-android-car'
+    }
+  ];
+
+
+
+  if (req.session && req.session.userId) {
     console.log('Getting user dashboard');
     var userId = req.params.userId;
     var user = {};
     User.find({
-    '_id': userId
-    } , function(err, usr) {
-    if (err) {
-      console.log("ERROR in finding user: " + err);
-      res.status(500).json({
-        'message': 'Internal Error in Finding User'
-      });
-    } else if (usr.length !== 0) {
-      user._id = usr[0]._id;
-      user.name = usr[0].name;
-      user.pass = usr[0].pass;
-      user.limit = usr[0].limit;
-      user.email = usr[0].email;
-      user.costs = usr[0].costs;
-      res.render('index', {
-        user: user
-      });
-    } else {
-      console.log("User Not Found!");
-      res.status(404).json({
-        "message": 'User Not Found'
-      });
-    }
-  });
+      '_id': userId
+    }, function(err, usr) {
+      if (err) {
+        console.log("ERROR in finding user: " + err);
+        res.status(500).json({
+          'message': 'Internal Error in Finding User'
+        });
+      } else if (usr.length !== 0) {
+        user._id = usr[0]._id;
+        user.name = usr[0].name;
+        user.pass = usr[0].pass;
+        user.limit = usr[0].limit;
+        if (typeof usr[0].total === 'undefined') {
+          usr[0].total = 0;
+        }
+        user.total = (usr[0].total).toFixed(2).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+        user.email = usr[0].email;
+        user.costs = usr[0].costs;
+
+        //calculate total for each category
+        user.costs.forEach(function(cost) {
+          var costCategory = cost.category;
+          //aadd amount to category total
+          categories.forEach(function(category) {
+            if (costCategory === category.name) {
+              category.total += cost.amount;
+            }
+          });
+        });
+
+        res.render('index', {
+          user: user,
+          categories: categories
+        });
+      } else {
+        console.log("User Not Found!");
+        res.status(404).json({
+          "message": 'User Not Found'
+        });
+      }
+    });
   } else {
     res.redirect("/login?error");
   }
