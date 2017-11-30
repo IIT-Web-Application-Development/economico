1724373df07f50db06d52d06ab757ee9d4e8fc7b
Grese Hyseni <ghyseni@hawk.iit.edu>
2017-11-30 02:18:32 -0600

    Disable default orderintg on dataTable init

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -501,7 +501,7 @@
         'paging': true,
         'lengthChange': true,
         'searching': true,
-        'ordering': true,
+        'ordering': false,
         'info': true,
         'autoWidth': true
       })
