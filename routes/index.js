d6dff9b61b5f96dcc17acf0847bec13d64152178
Grese Hyseni <ghyseni@hawk.iit.edu>
2017-11-30 17:37:29 -0600

    Show notification when limit is closed to be reached, or limit is bassed

--- routes/index.js
+++ routes/index.js
@@ -82,40 +82,70 @@ router.get('/dashboard/:userId', function(req, res, next) {
     var userId = req.params.userId;
     var user = {};
     User.find({
-        '_id': userId
-      }, function(err, usr) {
-        if (err) {
-          console.log("ERROR in finding user: " + err);
-          res.status(500).json({
-            'message': 'Internal Error in Finding User'
-          });
-        } else if (usr.length !== 0) {
-          user._id = usr[0]._id;
-          user.name = usr[0].name;
-          user.pass = usr[0].pass;
-          user.limit = usr[0].limit;
-          if (typeof usr[0].total === 'undefined') {
-            usr[0].total = 0;
-          }
-          user.total = (usr[0].total).toFixed(2).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
-          user.email = usr[0].email;
-          user.costs = usr[0].costs;
+      '_id': userId
+    }, function(err, usr) {
+      if (err) {
+        console.log("ERROR in finding user: " + err);
+        res.status(500).json({
+          'message': 'Internal Error in Finding User'
+        });
+      } else if (usr.length !== 0) {
+        user._id = usr[0]._id;
+        user.name = usr[0].name;
+        user.pass = usr[0].pass;
+        user.limit = usr[0].limit;
+        if (typeof usr[0].total === 'undefined') {
+          usr[0].total = 0;
+        }
+        user.total = usr[0].total;
+        user.email = usr[0].email;
+        user.costs = usr[0].costs;
 
-          //calculate total for each category
-          user.costs.forEach(function(cost) {
-              var costCategory = cost.category;
-              //aadd amount to category total
-              categories.forEach(function(category) {
-                  if (costCategory === category.name) {
-                    category.total = parseFloat(category.total + cost.amount).toFixed(2).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
-                }
-              });
+        //calculate total for each category
+        user.costs.forEach(function(cost) {
+          var costCategory = cost.category;
+          //aadd amount to category total
+          categories.forEach(function(category) {
+            if (costCategory === category.name) {
+              category.total = parseFloat(category.total + cost.amount).toFixed(2).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+            }
           });
+        });
+
+        //Send warning if user limit is reached/almost reached
+        var balance = parseFloat(user.limit) - parseFloat(user.total);
+
+        console.log('balance');
+        console.log(balance);
+        console.log('limit');
+        console.log(user.limit);
+        console.log('total');
+        console.log(user.total);
+
+        //Format money values
+        user.balance = parseFloat(balance).toFixed(2).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+        user.limit = parseFloat(user.limit).toFixed(2).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+        user.total = parseFloat(user.total).toFixed(2).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+
+        var notification;
+        if (balance <= 200 && balance > 0) {
+          notification = {};
+          notification.label = 'warning';
+          notification.textColor = 'orange';
+          notification.message = 'You have ' + user.balance + ' left before reaching your limit of $' + user.limit;
+        } else if (balance <= 0) {
+          notification = {};
+          notification.label = 'danger';
+          notification.textColor = 'red';
+          notification.message = 'You have negative balance of ' + user.balance + '. Please update your limit!';
+        }
+        user.notification = notification;
 
         res.render('index', {
           user: user,
-          categories: categories
+          categories: categories,
         });
+
       } else {
         console.log("User Not Found!");
         res.status(404).json({
@@ -123,9 +153,9 @@ router.get('/dashboard/:userId', function(req, res, next) {
         });
       }
     });
-} else {
-  res.redirect("/login?error");
-}
+  } else {
+    res.redirect("/login?error");
+  }
 });
 
 /* Get serrings page */
@@ -152,12 +182,33 @@ router.get('/settings/:userId', function(req, res, next) {
         if (typeof usr[0].total === 'undefined') {
           usr[0].total = 0;
         }
-        user.total = (usr[0].total).toFixed(2).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+        user.total = usr[0].total;
         user.email = usr[0].email;
         user.costs = usr[0].costs;
 
+        //Send warning if user limit is reached/almost reached
+        var balance = parseFloat(user.limit) - parseFloat(user.total);
+        //Format money values
+        user.balance = parseFloat(balance).toFixed(2).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+        user.limit = parseFloat(user.limit).toFixed(2).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+        user.total = parseFloat(user.total).toFixed(2).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+
+        var notification;
+        if (balance <= 200 && balance > 0) {
+          notification = {};
+          notification.label = 'warning';
+          notification.textColor = 'orange';
+          notification.message = 'You have ' + user.balance + ' left before reaching your limit of $' + user.limit;
+        } else if (balance <= 0) {
+          notification = {};
+          notification.label = 'danger';
+          notification.textColor = 'red';
+          notification.message = 'You have negative balance of ' + user.balance + '. Please update your limit!';
+        }
+        user.notification = notification;
+
         res.render('settings', {
-          user: user
+          user: user,
         });
       } else {
         console.log("User Not Found!");
