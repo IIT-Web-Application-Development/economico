75279e80cc3159c037338e4aff1893fa0feea6e6
milad <milad.sabouri@gmail.com>
2017-12-04 23:28:32 -0600

    testing unauthorized users

--- test/users.js
+++ test/users.js
@@ -12,8 +12,24 @@ chai.use(chaiHttp);
 describe('Users', () => {
     describe('/GET users', () => {
         it('it should return empty array when no user exists.', (done) => {
-			//TODO
-			expect(false, 'todo').to.be.true;
+			chai.request(app)
+            .get('/users')
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('array');
+                res.body.length.should.be.eql(0);
+                done();
+            });
+		});
+    });
+    describe('/GET users', () => {
+        it('it should return access denied for unauthorized user.', (done) => {
+			chai.request(app)
+            .get('/users')
+            .end((err, res) => {
+                res.should.have.status(403);
+                done();
+            });
 		});
     });
     describe('/POST users', () => {
@@ -22,10 +38,30 @@ describe('Users', () => {
 			expect(false, 'todo').to.be.true;
 		});
     });
+    describe('/POST users', () => {
+        it('it should return access denied for unauthorized user.', (done) => {
+			chai.request(app)
+            .post('/users')
+            .end((err, res) => {
+                res.should.have.status(403);
+                done();
+            });
+		});
+    });
     describe('/GET users/:userid', () => {
         it('it should return empty object when no user exists.', (done) => {
 			//TODO
 			expect(false, 'todo').to.be.true;
+		});
+    });
+    describe('/GET users/:userid', () => {
+        it('it should return access denied for unauthorized user.', (done) => {
+			chai.request(app)
+            .get('/users/:userid')
+            .end((err, res) => {
+                res.should.have.status(403);
+                done();
+            });
 		});
     });
     describe('/PUT users/:userid', () => {
@@ -34,10 +70,30 @@ describe('Users', () => {
 			expect(false, 'todo').to.be.true;
 		});
     });
+    describe('/PUT users/:userid', () => {
+        it('it should return access denied for unauthorized user.', (done) => {
+			chai.request(app)
+            .put('/users/:userid')
+            .end((err, res) => {
+                res.should.have.status(403);
+                done();
+            });
+		});
+    });
     describe('/DELETE users/:userid', () => {
         it('it should delete specific user', (done) => {
 			//TODO
 			expect(false, 'todo').to.be.true;
+		});
+    });
+    describe('/DELETE users/:userid', () => {
+        it('it should return access denied for unauthorized user.', (done) => {
+			chai.request(app)
+            .delete('/users/:userid')
+            .end((err, res) => {
+                res.should.have.status(403);
+                done();
+            });
 		});
     });
 });
